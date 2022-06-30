b0899a0d2655a34ab451c1c14497fbc1cfac601a
shvanasheikha <shvansheikha@gmail.com>
2022-06-30 18:22:48 +0430

    added GroupResource for api response

---
+++ app/Http/Resources/GroupResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class GroupResource extends JsonResource
+{
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'title' => $this->title,
+            'type' => $this->type,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
