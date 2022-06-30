cac8dd79d785834c68ea1e051eac7b7e33aafbe4
shvanasheikha <shvansheikha@gmail.com>
2022-06-30 12:29:42 +0430

    added api route for dashboard, card, group , product and games list

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,38 @@
 <?php
 
+use App\Http\Controllers\CardController;
+use App\Http\Controllers\GameController;
+use App\Http\Controllers\GroupController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::middleware('auth:sanctum')->get('/authenticated', function () {
+    return true;
+});
+
+Route::post('register', [RegisterController::class, 'register']);
+Route::post('login', [LoginController::class, 'login']);
+Route::post('logout', [LoginController::class, 'logout']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard');
+
+    // Cards Routes
+    Route::get('/cards', [CardController::class, 'index'])->name('cards.index');
+
+    // Cards Routes
+    Route::get('/groups', [GroupController::class, 'index'])->name('groups.index');
+
+    // Cards Routes
+    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+
+    // Cards Routes
+    Route::get('/games', [GameController::class, 'index'])->name('games.index');
 });
