a7b6e389b876f6913d570a89beed592876626709
shvanasheikha <shvansheikha@gmail.com>
2022-07-01 23:25:40 +0430

    added home page that contains some detail about site

---
+++ resources/js/components/Pages/Home.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="flex w-full font-mono">
+        <div class="w-full p-20">
+            Hey!<br>
+            This site help you to manage your coffee business!<br>
+            <h1>Don't forget this is a <span class="cursor-pointer text-blue-500 font-bold">Free</span> use site! enjoy
+                that!</h1>
+            Create some <span class="cursor-pointer text-blue-500">#Cards</span> and manage your customers by them.<br>
+            Each customer can order or play with their cards.<br>
+            Finally you can see what they ordered and how much should pay!<br>
+            The big point is you can see how many customer did you have each day! and what did they ordered<br>
+            Generally you can see history of your sells!<br>
+            You can groups your products and games.<br>
+            like <br>
+            - Coffee<br>
+            <div class="px-4">
+                - Espresso<br>
+                - Breve<br>
+                - Cafe Latte<br>
+                - Double Espress<br>
+            </div>
+            - PS4<br>
+            <div class="px-4">
+                - One Person PS4<br>
+                - Two Person PS4<br>
+                - Three Person PS4<br>
+            </div>
+            And customers can order some coffee by the card!<br>
+        </div>
+    </div>
+</template>
+<script>
+
+export default {
+    name: "Home",
+}
+</script>
