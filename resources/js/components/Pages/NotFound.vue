8bf38a255c9e2d9855dbee4816b685b1addeda06
shvanasheikha <shvansheikha@gmail.com>
2022-07-01 23:27:48 +0430

    added 404 page

---
+++ resources/js/components/Pages/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="text-gray-300 h-screen w-full flex items-center justify-center font-mono antialiased bg-gray-600">
+        <div>
+            <div>
+                <span>404 error</span>
+                <span>page not found</span>
+            </div>
+            <div>
+                <span>sorry about that!</span>
+                <router-link to="/" class="hover:text-blue-500">return home?</router-link>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {}
+</script>
