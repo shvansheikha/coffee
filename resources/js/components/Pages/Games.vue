438ae046a1204a938ed803d718c260267df1c1e8
shvanasheikha <shvansheikha@gmail.com>
2022-07-01 23:23:21 +0430

    added games page

---
+++ resources/js/components/Pages/Games.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="flex w-full font-mono">
+        <div
+            class="w-full px-4 pt-6 pb-20 md:px-40 md:pt-20 flex-grow h-screen flex flex-col shadow-inner overscroll-auto overflow-auto">
+
+            <div class="flex items-center pb-4 mb-4 border-b">
+                <input type="text"
+                       v-model="form.title"
+                       class="mr-4 form-control block w-1/4 px-3 py-1 hover:border-blue-600 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-400 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                       placeholder="Game Name">
+
+                <select
+                    v-model="form.group_id"
+                    class="form-select appearance-none mr-4 form-control block w-1/4 px-3 py-1 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-400 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white hover:border-blue-600 focus:border-blue-600 focus:outline-none">
+                    <option v-for="group in groupsList" v-bind:value="group.id">{{ group.title }}</option>
+                </select>
+
+                <input type="text"
+                       v-model="form.amount"
+                       class="mr-4 form-control block w-1/4 px-3 py-1 hover:border-blue-600 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-400 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                       placeholder="Amount Per Minute">
+
+                <div
+                    @click="storeGame"
+                    class="text-center border border-current hover:border-blue-500 transition-all duration-300 ease-in-out cursor-pointer hover:text-blue-500 rounded px-4 py-1">
+                    Create
+                </div>
+            </div>
+
+            <GamesTable
+                :groups-list="groupsList"
+                :games-list="gamesList"
+                @delete="getGamesList"
+                @update="getGamesList"/>
+        </div>
+    </div>
+</template>
+<script>
+
+import GamesTable from "../views/GamesTable";
+
+export default {
+    components: {GamesTable},
+    data() {
+        return {
+            gamesList: null,
+            groupsList: null,
+            form: {
+                title: null,
+                group_id: null,
+                amount: null,
+            }
+        }
+    },
+    created() {
+        this.getGroupsList();
+        this.getGamesList();
+    },
+    methods: {
+        getGroupsList() {
+            axios.get('groups', {params: {type: 'game'}})
+                .then((res) => {
+                    this.groupsList = res.data.data;
+                })
+        },
+        getGamesList() {
+            axios.get('games')
+                .then((res) => {
+                    this.gamesList = res.data.data;
+                })
+        },
+        storeGame() {
+            axios.post('games', this.form)
+                .then((res) => {
+                    if (res.status === 201) {
+                        this.getGamesList();
+                    }
+                    this.form.title = null;
+                    this.form.group_id = null;
+                    this.form.amount = null;
+                })
+                .catch((error) => {
+                    // console.log(error)
+                });
+        }
+    }
+}
+</script>
