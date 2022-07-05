31d087f4273210d6b5f449f3f648ec3a5fb645ee
shvanasheikha <shvansheikha@gmail.com>
2022-07-05 23:41:06 +0430

    rename file

--- app/Http/Requests/StoreCardRequest.php
+++ app/Http/Requests/StoreCardRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreCard extends FormRequest
+class StoreCardRequest extends FormRequest
 {
 
     public function authorize(): bool
