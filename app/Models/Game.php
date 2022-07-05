b1f0be0349d251913e31da9ec0573a537ad11378
shvanasheikha <shvansheikha@gmail.com>
2022-07-05 23:44:22 +0430

    added filter and user scope to game model

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Filters\GameFilters;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -27,5 +29,15 @@ class Game extends Model
     public function orders(): MorphMany
     {
         return $this->morphMany(Order::class, 'orderable');
+    }
+
+    public function scopeFilter($query, GameFilters $filters): Builder
+    {
+        return $filters->apply($query);
+    }
+
+    public function scopeOfUser($query, $user)
+    {
+        return $query->where('user_id', $user->id);
     }
 }
