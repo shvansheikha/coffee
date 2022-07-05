b6e78d71b8417ff798a866ed2f02a31b94bf190b
shvanasheikha <shvansheikha@gmail.com>
2022-07-05 23:42:16 +0430

    set background and hide menu on login page

--- resources/js/components/Pages/App.vue
+++ resources/js/components/Pages/App.vue
@@ -5,11 +5,10 @@
         <div
             v-if="showMenu"
             class="md:h-screen md:w-1/6 w-full px-4 bg-grey-100 w-48 flex-shrink-0 border-r border-gray-200 md:flex-col">
-            <!--            <Menu></Menu>-->
             <SideBar></SideBar>
         </div>
 
-        <div class="flex-grow h-screen flex flex-col shadow-inner overscroll-auto overflow-auto">
+        <div class="flex-grow h-screen flex flex-col shadow-inner overscroll-auto overflow-auto bg-gray-50">
             <router-view></router-view>
         </div>
     </div>
@@ -29,7 +28,7 @@ export default {
         },
         showMenu() {
             return !(
-                this.currentRouteName === 'login' ||
+                this.currentRouteName === 'Login' ||
                 this.currentRouteName === 'register' ||
                 this.currentRouteName === 'forgot-password' ||
                 this.currentRouteName === 'reset-password' ||
