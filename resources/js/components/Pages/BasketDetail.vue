9184de0248ec9d191935a174033158ad17168180
shvanasheikha <shvansheikha@gmail.com>
2022-07-06 21:25:54 +0430

    added basket form on BasketDetail.vue

--- resources/js/components/Pages/BasketDetail.vue
+++ resources/js/components/Pages/BasketDetail.vue
@@ -3,7 +3,13 @@
         <div class="w-full px-4 py-4 md:px-10 md:pt-6 lg:px-40 lg:pt-10">
 
             <div class="w-full md:flex justify-between">
-                @include('baskets.basket')
+                <div class="w-full bg-white cursor-pointer shadow rounded p-4 md:flex justify-between">
+                    <div><span>Card: </span><span> {{ basketObject.card_title }} </span></div>
+                    <div><span>Product: </span><span> {{ basketObject.products_price }} </span></div>
+                    <div><span>Game: </span><span> {{ basketObject.games_price }} </span></div>
+                    <div class="text-blue-500 uppercase font-bold">
+                        <span>Total: </span><span> {{ basketObject.total_price }} </span></div>
+                </div>
             </div>
 
             <div class="mt-4">
@@ -29,13 +35,24 @@ export default {
         return {
             gamesList: null,
             productsList: null,
+            basketObject: null,
         }
     },
     created() {
         this.getGamesOrder();
         this.getProductOrder();
+        this.getBasket();
     },
     methods: {
+        getBasket() {
+            axios.get('baskets/' + this.basket)
+                .then((res) => {
+                    this.basketObject = res.data.data;
+                })
+                .catch((error) => {
+                    console.log(error.response.data.errors)
+                });
+        },
         getGamesOrder() {
             axios.get('orders', {params: {type: 'game', basket: this.basket}})
                 .then((res) => {
