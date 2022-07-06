ec3d5ad6bae464a5c7eeda34ee48f9a42e16c5f4
shvanasheikha <shvansheikha@gmail.com>
2022-07-06 17:22:11 +0430

    add shadow to dashboard items

--- resources/js/components/Pages/Dashboard.vue
+++ resources/js/components/Pages/Dashboard.vue
@@ -4,7 +4,7 @@
             class="w-full px-4 pt-6 pb-20 md:px-40 md:pt-20 flex-grow h-screen flex flex-col shadow-inner overscroll-auto overflow-auto">
             <div v-for="item in daysList" @click="goToBaskets(item.closed_at)">
                 <div
-                    class="w-full bg-white md:flex border rounded my-2 py-2 px-6 justify-between cursor-pointer hover:border-blue-500 hover:text-blue-500">
+                    class="w-full shadow-sm bg-white md:flex border rounded my-2 py-2 px-6 justify-between cursor-pointer hover:border-blue-500 hover:text-blue-500">
                     <div class="w-1/3">date: {{ item.date }}</div>
                     <div class="w-1/3">total: {{ item.total }}</div>
                     <div class="w-1/3">total_price:{{ format(item.total_price) }}</div>
