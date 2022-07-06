54d639a0e170dd4e8e3bcad83a9cb6a18b70dc7d
shvanasheikha <shvansheikha@gmail.com>
2022-07-06 11:30:25 +0430

    refactore update function on CardController

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -2,14 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreCard;
-use App\Http\Requests\UpdateCard;
+use App\Http\Requests\StoreCardRequest;
+use App\Http\Requests\UpdateCardRequest;
 use App\Http\Resources\CardResource;
 use App\Models\Card;
-use App\Models\User;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class CardController extends Controller
@@ -31,13 +28,12 @@ class CardController extends Controller
         return CardResource::make($card);
     }
 
-    public function update(Card $card, UpdateCard $request): RedirectResponse
+    public function update(Card $card, UpdateCardRequest $request): CardResource
     {
         $card->update($request->validated());
 
-        return redirect()->route('cards.index');
+        return CardResource::make($card->fresh());
     }
-
 
     public function destroy(Card $card): JsonResponse
     {
