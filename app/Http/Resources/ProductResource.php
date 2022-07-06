928bc21241898a9f7a645a8030be225db0998853
shvanasheikha <shvansheikha@gmail.com>
2022-07-06 17:13:45 +0430

    put keys we need to ProductResource.php

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -6,8 +6,17 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class ProductResource extends JsonResource
 {
-    public function toArray($request)
+    public function toArray($request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'group_id' => $this->group_id,
+            'title' => $this->title,
+            'group' => $this->group->title,
+            'amount' => round($this->amount, 2),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+        ];
     }
 }
