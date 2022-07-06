71694735da29fa6a926c6d0ce0448eca984a3c46
shvanasheikha <shvansheikha@gmail.com>
2022-07-06 11:36:34 +0430

    added filter,ofUser and OwnProduct scope and diff attribute function to Order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+use App\Filters\OrderFilters;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -37,5 +40,42 @@ class Order extends Model
     public function game(): BelongsTo
     {
         return $this->belongsTo(Game::class, 'orderable_id');
+    }
+
+    public function scopeFilter($query, OrderFilters $filters): Builder
+    {
+        return $filters->apply($query);
+    }
+
+    public function scopeOfUser($query, $user)
+    {
+        return $query->whereUserId($user->id);
+    }
+
+    public function scopeOwnProduct($query)
+    {
+        return $query->where('orderable_type', get_class(new Product));
+    }
+
+    public function scopeOwnGame($query)
+    {
+        return $query->where('orderable_type', get_class(new Game));
+    }
+
+    public function getDiffAttribute(): ?string
+    {
+        if (!empty($this->stopped_at)) {
+            $from = Carbon::parse($this->started_at);
+            $to = Carbon::parse($this->stopped_at);
+            $diff_date = $to->diff($from);
+            $diff = "";
+            if ($diff_date->h > 0) {
+                $diff .= $diff_date->h . ":";
+            }
+            $diff .= $diff_date->i . ":" . $diff_date->s;
+            return $diff;
+        }
+
+        return null;
     }
 }
