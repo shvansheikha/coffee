565f8db39ea364ac7b7ecf534545ba54c93138ed
shvanasheikha <shvansheikha@gmail.com>
2022-07-07 18:12:59 +0430

    fix register page

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,25 +3,37 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Contracts\Auth\StatefulGuard;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
-    public function register(Request $request)
+    public function register(Request $request): JsonResponse
     {
-
         $request->validate([
             'name' => ['required'],
             'email' => ['required', 'email', 'unique:users'],
             'password' => ['required', 'min:6', 'confirmed']
         ]);
 
-        User::create([
+        $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password)
         ]);
+
+        $this->guard()->login($user);
+
+        return response()->json([]);
+    }
+
+    protected function guard(): Guard|StatefulGuard
+    {
+        return Auth::guard();
     }
 }
--- resources/js/components/Pages/Register.vue
+++ resources/js/components/Pages/Register.vue
@@ -1,38 +1,54 @@
 <template>
-    <div class="flex flex-wrap w-full justify-center items-center pt-56">
-        <div class="flex flex-wrap max-w-xl">
-            <div class="p-2 text-2xl text-gray-800 font-semibold"><h1>Register an account</h1></div>
-            <div class="p-2 w-full">
-                <label class="w-full" for="name">Name</label>
-                <span class="w-full text-red-500" v-if="errors.name">{{ errors.name[0] }}</span>
-                <input
-                    class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2"
-                    placeholder="Name" type="text" v-model="form.name">
+    <div class="flex w-full font-mono h-screen justify-between">
+        <div class="w-1/2 flex flex-wrap items-center justify-center">
+            <div>
+                <div class="p-2 text-2xl text-gray-800 font-semibold"><h1>Register an account</h1></div>
+                <div class="p-2 w-full">
+                    <label class="w-full" for="name">Name</label>
+                    <span class="w-full text-red-500" v-if="errors.name">{{ errors.name[0] }}</span>
+                    <input
+                        class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-blue-500 text-base px-4 py-2"
+                        placeholder="Name" type="text" v-model="form.name">
+                </div>
+                <div class="p-2 w-full">
+                    <label for="email">Your e-mail</label>
+                    <input
+                        class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-blue-500 text-base px-4 py-2"
+                        placeholder="Email" type="email" v-model="form.email">
+                </div>
+                <div class="p-2 w-full">
+                    <label for="password">Password</label>
+                    <input
+                        class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-blue-500 text-base px-4 py-2"
+                        placeholder="Password" type="password" v-model="form.password" name="password">
+                </div>
+                <div class="p-2 w-full">
+                    <label for="confirm_password">Confirm Password</label>
+                    <input
+                        class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-blue-500 text-base px-4 py-2"
+                        placeholder="Confirm Password" type="password" v-model="form.password_confirmation"
+                        name="password_confirmation">
+                </div>
+                <div class="p-2 w-full mt-4">
+                    <button @click.prevent="saveForm" type="submit"
+                            class="flex text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">
+                        Register
+                    </button>
+                    <div @click="goToLoginPage"
+                         class="w-full mt-4 cursor-pointer">
+                        Already have an account? <span class="text-blue-500">Login</span>
+                    </div>
+                </div>
+
             </div>
-            <div class="p-2 w-full">
-                <label for="email">Your e-mail</label>
-                <input
-                    class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2"
-                    placeholder="Email" type="email" v-model="form.email">
-            </div>
-            <div class="p-2 w-full">
-                <label for="password">Password</label>
-                <input
-                    class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2"
-                    placeholder="Password" type="password" v-model="form.password" name="password">
-            </div>
-            <div class="p-2 w-full">
-                <label for="confirm_password">Confirm Password</label>
-                <input
-                    class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2"
-                    placeholder="Confirm Password" type="password" v-model="form.password_confirmation"
-                    name="password_confirmation">
-            </div>
-            <div class="p-2 w-full mt-4">
-                <button @click.prevent="saveForm" type="submit"
-                        class="flex text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+        </div>
+
+        <div class="w-1/2 flex flex-col" style="background-image: url('img/bg-login.jpg'); background-size: cover;">
+            <div
+                class="mt-auto p-8 text-white mb-20 mx-20 bg-gray-300 h-1/3 rounded bg-opacity-40 backdrop-blur border border-gray-200">
+                <span class="text-lg font-bold">
                     Register
-                </button>
+                </span>
             </div>
         </div>
     </div>
@@ -54,9 +70,13 @@ export default {
         saveForm() {
             axios.post('register', this.form).then(() => {
                 console.log('saved');
+                this.$router.push({name: "Dashboard"});
             }).catch((error) => {
                 this.errors = error.response.data.errors;
             })
+        },
+        goToLoginPage() {
+            this.$router.push({name: "Login"});
         }
     }
 }
