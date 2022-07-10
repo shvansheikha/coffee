c410352acf3e10701184526149a4f60d96b0f206
shvanasheikha <shvansheikha@gmail.com>
2022-07-10 13:18:30 +0430

    remove unused blades

--- resources/views/baskets/basket.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="w-full shadow rounded p-4 md:flex justify-between">
-    <div><span>Card: </span><span> {{$basket->card->title}} </span></div>
-    <div><span>Product: </span><span> {{$basketData->products}} </span></div>
-    <div><span>Game: </span><span> {{$basketData->games}} </span></div>
-    <div class="text-blue-500 uppercase font-bold"><span>Total: </span><span> {{$basketData->total}} </span></div>
-</div>
--- resources/views/printer/basket.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="w-full bg-gray-50 py-2 flex justify-between">
-    <div>Card: {{$basket->card->title}}</div>
-    <div>Product: {{$basketData->products}}</div>
-    <div>Game: {{$basketData->games}}</div>
-    <div class="uppercase">Total: {{$basketData->total}} </div>
-</div>
