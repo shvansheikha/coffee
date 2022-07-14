52326ac27301ed838e70faab50c40ca228c6112c
shvanasheikha <shvansheikha@gmail.com>
2022-07-14 13:48:51 +0430

    make games page responsive

--- resources/js/components/Pages/Games.vue
+++ resources/js/components/Pages/Games.vue
@@ -1,23 +1,22 @@
 <template>
     <div class="flex w-full font-mono">
         <div
-            class="w-full px-4 pt-6 pb-20 md:px-40 md:pt-20 flex-grow h-screen flex flex-col shadow-inner overscroll-auto overflow-auto">
-
-            <div class="flex items-center pb-4 mb-4 border-b">
+            class="w-full px-8 pt-6 pb-20 md:px-10 lg:px-20 md:pt-20 flex-grow h-screen flex flex-col shadow-inner overscroll-auto overflow-auto">
+            <div class="md:flex items-center pb-4 mb-4 border-b">
                 <input type="text"
                        v-model="form.title"
-                       class="mr-4 form-control block w-1/4 px-3 py-1 hover:border-blue-600 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-400 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                       class="mb-4 md:mb-0 w-full mr-4 form-control block md:w-1/4 px-3 py-1 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-400 rounded transition ease-in-out m-0 focus:text-gray-700 hover:border-blue-600 focus:bg-white focus:border-blue-600 focus:outline-none"
                        placeholder="Game Name">
 
                 <select
                     v-model="form.group_id"
-                    class="form-select appearance-none mr-4 form-control block w-1/4 px-3 py-1 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-400 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white hover:border-blue-600 focus:border-blue-600 focus:outline-none">
+                    class="mb-4 md:mb-0 w-full form-select appearance-none mr-4 form-control block md:w-1/4 px-3 py-1 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-400 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white hover:border-blue-600 focus:border-blue-600 focus:outline-none">
                     <option v-for="group in groupsList" v-bind:value="group.id">{{ group.title }}</option>
                 </select>
 
                 <input type="text"
                        v-model="form.amount"
-                       class="mr-4 form-control block w-1/4 px-3 py-1 hover:border-blue-600 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-400 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                       class="mb-4 md:mb-0 w-full mr-4 form-control block md:w-1/4 px-3 py-1 hover:border-blue-600 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-400 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                        placeholder="Amount Per Minute">
 
                 <div
--- resources/js/components/views/GamesTable.vue
+++ resources/js/components/views/GamesTable.vue
@@ -1,62 +1,85 @@
 <template>
     <div>
-        <table class="w-full text-sm text-left text-gray-500 cursor-pointer shadow rounded">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-50">
-            <tr>
-                <th scope="col" class="px-6 py-3">
+        <div class="w-full bg-white text-sm text-left text-gray-500 cursor-pointer rounded-md shadow-lg">
+            <div
+                class="text-gray-700 uppercase bg-gray-50 border-b md:flex justify-between font-medium rounded-t-md hidden md:visible">
+
+                <div class="text-center px-3 py-3 md:w-1/12 lg:w-1/12">
                     #
-                </th>
-                <th scope="col" class="px-6 py-3">
+                </div>
+
+                <div class="px-3 py-3 md:w-4/12 lg:w-4/12">
                     Title
-                </th>
-                <th scope="col" class="px-6 py-3">
+                </div>
+
+                <div class="px-3 py-3 md:w-3/12 lg:w-3/12">
                     Category
-                </th>
-                <th scope="col" class="px-6 py-3">
+                </div>
+
+                <div class="px-3 py-3 md:w-2/12 lg:w-2/12">
                     Amount
-                </th>
-                <th scope="col" class="px-6 py-3">
+                </div>
+
+                <div class="px-3 py-3 md:w-1/12 lg:w-1/12 items-center flex justify-center">
                     <span>Edit</span>
-                </th>
+                </div>
 
-                <th scope="col" class="px-6 py-3">
+                <div class="px-3 py-3 md:w-1/12 lg:w-1/12 items-center flex justify-center">
                     Delete
-                </th>
-            </tr>
-            </thead>
-            <tbody>
+                </div>
+            </div>
 
+            <div v-for="(game, index) in gamesList"
+                 class="w-full hover:bg-gray-50 md:flex md:justify-between font-medium"
+                 :class="(index === gamesList.length -1 )?'':'border-b'">
 
-            <tr v-for="(game, index) in gamesList" class="bg-white border-b hover:bg-gray-50">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                    {{ (index) + 1 }}
-                </th>
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                    {{ game.title }}
-                </th>
-                <td class="px-6 py-4">
-                    {{ game.group }}
-                </td>
-                <td class="px-6 py-4">
-                    {{ game.amount }}
-                </td>
-                <td class="px-6 py-3">
-                    <div @click="updateForm(game)" class="hover:text-blue-500 uppercase">
-                        edite
+                <div class="hidden md:block text-center px-3 py-3 md:w-1/12 lg:w-1/12">
+                    {{ index + 1 }}
+                </div>
+
+                <div class="px-3 py-3 md:w-4/12 lg:w-4/12 ">
+                    <span class="md:hidden uppercase text-gray-700 mr-2 font-medium">Title:</span>
+                    <span>{{ game.title }}</span>
+                </div>
+
+                <div class="px-3 py-3 md:w-3/12 lg:w-3/12">
+                    <span class="md:hidden uppercase text-gray-700 mr-2 font-medium">Category:</span>
+                    <span>{{ game.group }}</span>
+                </div>
+
+                <div class="px-3 py-3 md:w-2/12 lg:w-2/12">
+                    <span class="md:hidden uppercase text-gray-700 mr-2 font-medium">Amount:</span>
+                    <span>{{ game.amount }}</span>
+                </div>
+
+                <div class="md:w-2/12 flex m-2 md:m-0 rounded-md border md:border-none">
+                    <div
+                        @click="updateForm(game)"
+                        class="w-1/2 px-3 py-2 rounded-l-md md:rounded-none bg-blue-500 text-gray-200 md:text-inherit md:bg-inherit items-center flex justify-center hover:text-gray-800 md:hover:text-blue-500 uppercase">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
+                             viewBox="0 0 24 24"
+                             stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                  d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/>
+                        </svg>
                     </div>
-                </td>
 
-                <td class="px-6 py-4">
-                    <div @click="confirmDelete(game)" class="hover:text-blue-500">
-                        Delete
+                    <div
+                        @click="confirmDelete(game)"
+                        class="w-1/2 px-3 py-2 rounded-r-md md:rounded-none bg-gray-800 text-gray-200 md:text-inherit md:bg-inherit items-center flex justify-center hover:text-blue-500 uppercase">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
+                             viewBox="0 0 24 24"
+                             stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
+                        </svg>
                     </div>
-                </td>
-            </tr>
-            </tbody>
-        </table>
+                </div>
+            </div>
+        </div>
 
         <Modal :showing="modalShowing" @close="modalShowing = false">
-            <h2 class="text-lg font-bold text-gray-900">Delete Game</h2>
+            <h2 class="text-lg font-bold text-gray-900">Delete Product</h2>
             <span class="mb-6 text-sm">
                 Do you really want to delete
                 <span class="text-red-600">{{ deleteItem.title }}</span>?
@@ -64,13 +87,13 @@
 
             <div class="flex mt-8">
                 <button
-                    class="px-4 py-1 rounded border hover:border-blue-500 hover:text-blue-500"
+                    class="px-4 py-1 rounded-md border hover:border-blue-500 hover:text-blue-500"
                     @click="deleteGame(deleteItem.id)">
                     Yes
                 </button>
 
                 <button
-                    class="px-4 py-1 rounded border ml-4 hover:border-blue-500 hover:text-blue-500"
+                    class="px-4 py-1 rounded-md border ml-4 hover:border-blue-500 hover:text-blue-500"
                     @click="modalShowing = false">
                     Close
                 </button>
@@ -80,15 +103,15 @@
         <Modal :showing="updateModalShowing" @close="updateModalShowing = false">
             <h2 class="text-lg font-bold text-gray-900">Update Group</h2>
 
-            <div class="mt-8 rounded flex items-center pb-4">
+            <div class="mt-8 rounded-md md:flex w-80 items-center pb-4">
                 <input type="text"
                        v-model="form.title"
-                       class="mr-4 form-control block w-80 px-3 py-1 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-400 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                       class="mr-4 mt-4 md:mt-0 form-control block w-full md:w-2/4 px-3 py-1 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-400 rounded-md transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                        placeholder="Product Title">
 
                 <select
                     v-model="form.group_id"
-                    class="form-select appearance-none mr-4 form-control block w-1/4 px-3 py-1 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-400 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white hover:border-blue-600 focus:border-blue-600 focus:outline-none">
+                    class="form-select appearance-none mr-4 mt-4 md:mt-0 form-control block w-full md:w-1/4 px-3 py-1 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-400 rounded-md transition ease-in-out m-0 focus:text-gray-700 focus:bg-white hover:border-blue-600 focus:border-blue-600 focus:outline-none">
                     <option v-for="group in groupsList"
                             v-bind:value="group.id"
                             :selected="group.id === updateItem.group_id">
@@ -99,26 +122,29 @@
                 <input type="text"
                        name="amount"
                        v-model="form.amount"
-                       class="mr-4 form-control block w-80 px-3 py-1 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-400 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                       class="mr-4 mt-4 md:mt-0 form-control block w-full md:w-1/4 px-3 py-1 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-400 rounded-md transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                        placeholder="Product Amount">
             </div>
 
             <div class="flex mt-8">
                 <button
-                    class="px-4 py-1 rounded border hover:border-blue-500 hover:text-blue-500"
+                    class="px-4 py-1 rounded-md border hover:border-blue-500 hover:text-blue-500"
                     @click="updateGame(updateItem.id)">
                     Update
                 </button>
 
                 <button
-                    class="px-4 py-1 rounded border ml-4 hover:border-blue-500 hover:text-blue-500"
+                    class="px-4 py-1 rounded-md border ml-4 hover:border-blue-500 hover:text-blue-500"
                     @click="updateModalShowing = false">
                     Close
                 </button>
             </div>
         </Modal>
+
     </div>
+
 </template>
+
 <script>
 import Modal from "../utility/Modal";
 
