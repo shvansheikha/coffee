6467afa8aad82c0d7341f08223dfe15021eca7f0
shvanasheikha <shvansheikha@gmail.com>
2022-07-15 15:40:35 +0430

    make responsive register page

--- resources/js/components/Pages/Register.vue
+++ resources/js/components/Pages/Register.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="flex w-full font-mono h-screen justify-between bg-white">
-        <div class="w-1/2 flex flex-wrap items-center justify-center">
-            <div class="w-1/2">
+    <div class="md:flex w-full font-mono h-screen justify-between">
+        <div class="md:w-1/2 flex flex-wrap items-center justify-center">
+            <div class="mx-10 lg:mx-0 w-full mt-20 lg:mt-0 lg:w-1/2">
                 <div class="p-2 text-2xl text-gray-800 font-semibold"><h1>Register an account</h1></div>
                 <div class="mt-4 w-full">
                     <label class="text-gray-400" for="name">Name</label>
@@ -34,7 +34,7 @@
                             class="w-full text-white bg-gray-800 text-center border-0 py-2 px-8 focus:outline-none hover:bg-gray-600 rounded-md">
                         Register
                     </button>
-                    <div @click="goToLoginPage"
+                    <div @click="goToPage('Login')"
                          class="w-full mt-4 cursor-pointer">
                         Already have an account? <span class="text-blue-500">Login</span>
                     </div>
@@ -42,12 +42,13 @@
             </div>
         </div>
 
-        <div class="w-1/2 flex flex-col" style="background-image: url('img/bg-login.jpg'); background-size: cover;">
+        <div class="hidden md:block w-1/2 md:flex md:flex-col" style="background-image: url('img/bg-login.jpg'); background-size: cover;">
             <div
                 class="mt-auto p-8 text-white mb-20 mx-20 bg-gray-300 h-1/3 rounded bg-opacity-40 backdrop-blur border border-gray-200">
-                <span class="text-lg font-bold">
-                    Register
-                </span>
+                <div class="text-lg">
+                    Do you want to know about this site? Go to
+                    <span class="font-bold cursor-pointer hover:text-gray-700" @click="goToPage('Home')">Home Page</span>
+                </div>
             </div>
         </div>
     </div>
@@ -74,8 +75,8 @@ export default {
                 this.errors = error.response.data.errors;
             })
         },
-        goToLoginPage() {
-            this.$router.push({name: "Login"});
+        goToPage(page) {
+            this.$router.push({name: page});
         }
     }
 }
