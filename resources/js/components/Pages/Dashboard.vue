17d6e979de7201ea206e8ce96f2ce7f4d9897c2f
shvanasheikha <shvansheikha@gmail.com>
2022-07-23 13:32:57 +0430

    use div instead of table in dashboard page

--- resources/js/components/Pages/Dashboard.vue
+++ resources/js/components/Pages/Dashboard.vue
@@ -1,15 +1,59 @@
 <template>
     <div class="flex w-full font-mono">
+
         <div
-            class="w-full px-4 pt-6 pb-20 md:px-40 md:pt-20 flex-grow h-screen flex flex-col shadow-inner overscroll-auto overflow-auto">
-            <div v-for="item in daysList" @click="goToBaskets(item.closed_at)">
+            class="w-full px-8 pt-6 pb-20 md:px-10 lg:px-20 md:pt-20 flex-grow h-screen flex flex-col overscroll-auto overflow-auto">
+
+            <div
+                class="w-full md:bg-white text-sm text-left text-gray-500 cursor-pointer md:rounded-md md:shadow-md">
+
+
                 <div
-                    class="w-full shadow-sm bg-white md:flex border rounded my-2 py-2 px-6 justify-between cursor-pointer hover:border-blue-500 hover:text-blue-500">
-                    <div class="w-1/3">date: {{ item.date }}</div>
-                    <div class="w-1/3">total: {{ item.total }}</div>
-                    <div class="w-1/3">total_price:{{ format(item.total_price) }}</div>
+                    class="text-gray-700 uppercase bg-gray-50 border-b md:flex justify-between font-medium rounded-t-md hidden md:visible">
+
+                    <div class="text-center px-3 py-3 md:w-1/12">
+                        #
+                    </div>
+
+                    <div class="px-3 py-3 md:w-4/12">
+                        Date
+                    </div>
+
+                    <div class="px-3 py-3 md:w-3/12">
+                        Baskets
+                    </div>
+
+                    <div class="px-3 py-3 md:w-3/12">
+                        Total Price
+                    </div>
+                </div>
+
+                <div v-for="(item, index) in daysList"
+                     @click="goToBaskets(item.closed_at)"
+                     class="w-full hover:bg-gray-50 md:flex md:justify-between font-medium mb-4 md:mb-0 pb-2 md:pb-0 shadow-lg md:shadow-none bg-white md:bg-transparent rounded-md md:rounded-none"
+                     :class="(index === daysList.length -1 )?'md:hover:rounded-b-md':'md:border-b'">
+
+                    <div class="hidden md:block text-center px-3 py-3 md:w-1/12">
+                        {{ index + 1 }}
+                    </div>
+
+                    <div class="px-3 py-3 md:w-4/12">
+                        <span class="md:hidden uppercase text-gray-700 mr-2 font-medium">Date:</span>
+                        <span>{{ item.date }}</span>
+                    </div>
+
+                    <div class="px-3 py-3 md:w-3/12">
+                        <span class="md:hidden uppercase text-gray-700 mr-2 font-medium">Baskets:</span>
+                        <span>{{ item.total }}</span>
+                    </div>
+
+                    <div class="px-3 py-3 md:w-3/12">
+                        <span class="md:hidden uppercase text-gray-700 mr-2 font-medium">Total Price:</span>
+                        <span>{{ format(item.total_price) }}</span>
+                    </div>
                 </div>
             </div>
+
         </div>
     </div>
 </template>
